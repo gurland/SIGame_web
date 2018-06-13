f349018efb2147f843cd19fe1fa6095c564d0535
gurland <stas.gurland@gmail.com>
2018-06-13 17:01:46 +0300

    Add index template rendering

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Registration template. Trust me! lolkek"
+    return render_template('index.html')
