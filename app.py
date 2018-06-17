0a96e0535fe8d6d46bd3e842567bde78e9c5b182
gurland <stas.gurland@gmail.com>
2018-06-18 02:45:25 +0300

    Revert "Rename template filename from "index.html" to "login.html""
    This reverts commit 0927ea1

--- app.py
+++ app.py
@@ -1,37 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for
-from flask_login import LoginManager, UserMixin
-from db_manager import Users
-
+from flask import Flask, render_template
 app = Flask(__name__)
-lm = LoginManager()
-lm.init_app(app)
-
-if not Users.table_exists():
-    Users.create_table()
 
 
 @app.route("/")
-def index():
-    return render_template('login.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    pass
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if not request.method == 'POST':
-        return redirect(url_for('index'))
-
-    login = request.form['login']
-    email = request.form['email']
-    password = request.form['password']
-
-    Users.create(login=login, email=email, password=password)
-
-    return redirect(url_for('index'))
-
-
-app.run(debug=True)
+def hello():
+    return render_template('index.html')
