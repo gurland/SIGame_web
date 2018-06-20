e21eee6cfd6f640a2ed6f8636cceb0987452a54e
dvoyakiy <dvoyakiyyy@gmail.com>
2018-06-20 23:15:29 +0300

    Add views for '/login' and '/register'

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_login import LoginManager, UserMixin
 from db_manager import Users
 
 app = Flask(__name__)
-
+lm = LoginManager()
+lm.init_app(app)
 
 if not Users.table_exists():
     Users.create_table()
@@ -13,4 +15,23 @@ def index():
     return render_template('auth.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if not request.method == 'POST':
+        return redirect(url_for('index'))
+
+    login = request.form['login']
+    email = request.form['email']
+    password = request.form['password']
+
+    Users.create(login=login, email=email, password=password)
+
+    return redirect(url_for('index'))
+
+
+app.run(debug=True)
