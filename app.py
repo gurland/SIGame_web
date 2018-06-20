728560e284a20acebcf0f66a4d1528acd954c6ba
dvoyakiy <dvoyakiyyy@gmail.com>
2018-06-20 23:14:15 +0300

    Create and connect peewee user model

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from db_manager import Users
+
 app = Flask(__name__)
 
 
+if not Users.table_exists():
+    Users.create_table()
+
+
 @app.route("/")
-def hello():
+def index():
     return render_template('auth.html')
+
+
+
