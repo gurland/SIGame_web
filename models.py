15ed86a70553c9450b11721cd4bf88363b88e143
gurland <stas.gurland@gmail.com>
2018-06-22 19:40:47 +0300

    Add password hashing and create basemodel

--- models.py
+++ models.py
@@ -1,15 +1,25 @@
-from peewee import SqliteDatabase, Model, CharField
+from peewee import SqliteDatabase, Model, CharField, TextField
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 db = SqliteDatabase('users.db')
 
 
-class Users(Model):
-    login = CharField(unique=True, null=False)
-    email = CharField(unique=True, null=False)
-    password = CharField(unique=False, null=False)
-
+class BaseModel(Model):
     class Meta:
         database = db
+
+
+class Users(BaseModel):
+    login = CharField(unique=True)
+    email = CharField(unique=True)
+    pw_hash = TextField()
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
 
 
 db.create_tables([Users], safe=True)
