00de273981481c5d19a10d775be06a84fe2b5b1b
gurland <stas.gurland@gmail.com>
2018-06-22 18:22:10 +0300

    Remove explicit id field and rename file

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
-from peewee import SqliteDatabase, Model, IntegerField, CharField
+from peewee import SqliteDatabase, Model, CharField
 
 db = SqliteDatabase('users.db')
 
 
 class Users(Model):
-    id = IntegerField(primary_key=True, unique=True, null=False)
     login = CharField(unique=True, null=False)
     email = CharField(unique=True, null=False)
     password = CharField(unique=False, null=False)
