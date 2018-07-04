1498dacf43d3019fbd6628faae9cbb00104e0f76
dvoyakiy <dvoyakiyyy@gmail.com>
2018-07-04 20:22:32 +0300

    Add profile tab switches

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     let regBox = $('#registration-box');
     let authBox = $('#authorization-box');
+    let chatBox = $('#live-chat');
+    let sendForm = $('#send-form');
+    let profileBox = $('.user-profile');
+
+    // profileBox.hide();
 
     $('#auth_btn').click(function () {
         regBox.hide();
@@ -10,5 +15,19 @@ $(document).ready(function () {
     $('#reg_btn').click(function () {
         authBox.hide();
         regBox.show();
-    })
+    });
+
+    $('#profile-tab').click(function(){
+        chatBox.hide();
+        sendForm.hide();
+        profileBox.show();
+    });
+
+    $('#chat-tab').click(function(){
+        chatBox.show();
+        sendForm.show();
+        profileBox.hide();
+    });
+
+
 });
