552022070883a85663fb63b84b3dc9ad7697ee1e
gurland <stas.gurland@gmail.com>
2018-07-09 00:49:43 +0300

    Add SMTP settings, salt for token generation and domain

--- app/config.py
+++ app/config.py
@@ -1,1 +1,12 @@
 DEBUG = True  # TODO: Change this on release
+SECURITY_PASSWORD_SALT = 'secret'
+CURRENT_WEBSITE = 'http://127.0.0.1'
+
+# SMTP settings
+
+SMTP_HOST = 'smtp.gmail.com'
+SMTP_PORT = 587
+
+SMTP_EMAIL = 'perfectgerund@gmail.com'
+SMTP_LOGIN = 'perfectgerund'
+SMTP_PASSWORD = 'papara1111'
