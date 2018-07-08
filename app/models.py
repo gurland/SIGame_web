2166189fafff440e92c43952b8aeaf0ebda86ae1
gurland <stas.gurland@gmail.com>
2018-07-09 00:50:21 +0300

    Add 4 methods to support Flask-Login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from peewee import SqliteDatabase, Model, CharField, TextField
+from peewee import SqliteDatabase, Model, CharField, TextField, BooleanField
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -14,6 +14,20 @@ class User(BaseModel):
     login = CharField(unique=True)
     email = CharField(unique=True)
     pw_hash = TextField()
+    authenticated = BooleanField(default=False)
+    active = BooleanField(default=False)
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.login
 
     def set_password(self, password):
         self.pw_hash = generate_password_hash(password)
