e90b6d4930cab3108e77c8b4e5d906724e8361cf
gurland <stas.gurland@gmail.com>
2018-07-09 00:49:14 +0300

    Add support for local launch

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -1,5 +1,5 @@
 import os
 
-broker_url = os.environ.get('BROKER_URL', '')
-result_backend = os.environ.get('CELERY_RESULT_BACKEND', 'rpc://')
+broker_url = os.environ.get('BROKER_URL', 'redis://localhost:6379/0')
+result_backend = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
