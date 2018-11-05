d3707b0d1f9720b7b7134f3ef27b604144923cd5
dvoyakiy <dvoyakiyyy@gmail.com>
2018-11-05 16:13:42 +0200

    Add package list

--- src/lobby/creation-menu/PackagesList.js
+++ src/lobby/creation-menu/PackagesList.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react'
 
+import Package from './Package'
 import './PackagesList.css'
-import Button from "../../Button";
 
 export default class PackagesList extends Component{
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            selectedPackageId: 0
+
+        };
+
+        this.handlePackageClick = this.handlePackageClick.bind(this);
+    }
+
+    handlePackageClick(id){
+        this.setState({
+            selectedPackageId: id
+        });
+    }
 
     render() {
         return (
@@ -12,11 +27,18 @@ export default class PackagesList extends Component{
                 <div className={'section-title'}>
                     <span>Доступные пакеты</span>
                 </div>
-                <div id="package-list-list">
-
+                <div id="package-list">
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'1'} packageName={'Аниме пафівфвфвкет'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'2'} packageName={'Аниме пафівфвфівфівфікет'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'3'} packageName={'Аниме пакфівфвфіветasasasaasdadadd'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'4'} packageName={'Аниме adasdassddasdіваіваіваівааіва'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'5'} packageName={'Аниме пафічфasdadasdasdфвфвівічкет'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'6'} packageName={'Аниме пакічфчфчіет'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'7'} packageName={'Аниме пакеясячсівсччт'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'8'} packageName={'Аниме пакевфівфвфвіфвіфівт'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'9'} packageName={'Аниме пакфіефівфівфівт'}/>
+                    <Package selectedPackageId={this.state.selectedPackageId} onPackageClick={(e) => (this.handlePackageClick(e.target.id))} packageId={'10'} packageName={'Аниме пачкчфівфвфвіфет'}/>
                 </div>
-                <Button id={'choose-package-button'} btnText={'Выбрать пакет'}/>
-
             </div>
         );
     }
